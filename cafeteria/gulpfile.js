4883e8a32054dfb75c9affa9ce9ab4a09d9d6563
Caballero018 <alejoxcaballero18@gmail.com>
2023-07-11 12:02:46 -0500

    Update

--- cafeteria/gulpfile.js
+++ cafeteria/gulpfile.js
@@ -5,9 +5,6 @@ const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
-// Imagenes
-const imagemin = require('gulp-imagemin');
-
 function css(done) {
     // Compilar SASS
     // Pasos: 1- identificar archivo, 2 - Compilarla, 3 - Guardar el .css
@@ -20,17 +17,11 @@ function css(done) {
     done();
 }
 
-function imagenes() {
-    return src('src/img/**/*')
-        .pipe(dest('build/img'));
-}
 
 function dev() {
     watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.imagenes = imagenes;
-exports.default = series(imagenes, css, dev)
+exports.default = series(css, dev)
